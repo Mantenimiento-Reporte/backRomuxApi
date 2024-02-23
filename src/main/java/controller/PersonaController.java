7a4826322e2017e00d1ee80f82ae258464c0c73a
Cristian David Ospina Ospina <100448542+cristianospina@users.noreply.github.com>
2024-02-22 19:33:57 -0500

    Dependencias
    Dependencias

--- src/main/java/controller/PersonaController.java
+++ src/main/java/controller/PersonaController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.PersonaService;
 @RestController
+@RequestMapping("v1")
 
 public class PersonaController {
 
