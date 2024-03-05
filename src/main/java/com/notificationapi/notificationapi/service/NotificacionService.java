bd46033a986ab829a9ca31d224d0aaad351a27bf
DanielGarQ <danielgarciaquiceno@gmail.com>
2024-03-04 20:32:40 -0500

    Conexion DB

--- src/main/java/com/notificationapi/notificationapi/service/NotificacionService.java
+++ src/main/java/com/notificationapi/notificationapi/service/NotificacionService.java
@@ -7,10 +7,8 @@ import com.notificationapi.notificationapi.entity.UsuarioEntity;
 import com.notificationapi.notificationapi.repository.NotificacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
